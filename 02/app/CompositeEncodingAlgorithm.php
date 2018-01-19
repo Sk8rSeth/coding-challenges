da26fe1d71f340fa8ca449542e16d0a72082e516
Sk8rSeth <sethhowell2197@gmail.com>
2018-01-18 20:45:07 -0700

    added composite algorithm

--- 02/app/CompositeEncodingAlgorithm.php
+++ 02/app/CompositeEncodingAlgorithm.php
@@ -34,10 +34,16 @@ class CompositeEncodingAlgorithm implements EncodingAlgorithm
      */
     public function encode($text)
     {
-        /**
-         * @todo: Implement it
-         */
+        // not sure if im supposed to be adding the other algorithms directly or not
+         $this->add('OffsetEncodingAlgorithm');
+         $this->add('SubstitutionEncodingAlgorithm');
 
-        return '';
+        $completed_text = $text;
+         foreach ($this->algorithms as $algorithm) {
+           $instance = new $algorithm;
+           $completed_text = $instance->encode($completed_text); // the '=' replaces the string with the newly encoded value for each loop
+         }
+
+        return $completed_text; // will return text after it has been repeated encoded with the entire list of algorithms in array
     }
 }
